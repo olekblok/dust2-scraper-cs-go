00355385dd8463f04d32dd8f6d14510c23cd0d17
olekblok <blokaleksander@gmail.com>
2024-06-07 11:15:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+def load_user_agents(file_path): 
+        with open(self.ua_file, 'r') as f:
+            return f.read().splitlines()
+
+
+def main():
